66c6a3936dc62e95ec83458481b91d21ddce0009
AD <66907615+add830830@users.noreply.github.com>
2020-07-07 01:44:34 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -299,7 +299,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user, e
                     reply_to_message_id=message.reply_to_message.message_id,
                     progress=progress_for_pyrogram,
                     progress_args=(
-                        "trying to upload",
+                        "Uploading To Telegram",
                         message_for_progress_display,
                         start_time
                     )
